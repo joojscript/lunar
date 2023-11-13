66e790d57ee41ac5b85f4e1e35c707a72be48e8a
João Augusto Perin de Souza <joaoaugustoperin@gmail.com>
2023-11-13 09:27:18 -0300

    Add option to read the keys from env

--- apps/server/src/modules/auth/auth.constants.ts
+++ apps/server/src/modules/auth/auth.constants.ts
@@ -1,5 +1,7 @@
 import { readFileSync } from 'fs';
 
-export const privateKey = readFileSync('keys/private.pem');
+export const privateKey =
+  readFileSync('keys/private.pem') ?? process.env.PRIVATE_KEY;
 
-export const publicKey = readFileSync('keys/public.pem');
+export const publicKey =
+  readFileSync('keys/public.pem') ?? process.env.PUBLIC_KEY;
